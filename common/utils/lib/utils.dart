9193b7e5c3e346417d3150901d080042a82a0f57
Max Size <maximuhkass14@gmail.com>
2024-10-14 19:59:38 +0300

    рефакторинг

--- common/utils/lib/utils.dart
+++ common/utils/lib/utils.dart
@@ -1,7 +1,4 @@
 library utils;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/either/either.dart';
+export 'src/usecase/usecase.dart';
