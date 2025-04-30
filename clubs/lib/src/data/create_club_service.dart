13023daa0a8ad2fee0c949f43a843b0c3073551e
oleole23 <125196739+oleole23@users.noreply.github.com>
2025-04-30 18:41:15 +0300

    подключение API создания группы

--- clubs/lib/src/data/create_club_service.dart
+++ clubs/lib/src/data/create_club_service.dart
@@ -3,47 +3,68 @@ import 'package:logger/logger.dart';
 import 'package:core/core.dart';
 
 class ClubApiService {
-  final Dio httpClient = Dio();
   final dioClient = NetworkConfig.instance.dio;
 
-  final String url = 'https://random-walk.ru:44424/clubs';
+  final String url = 'https://random-walk.ru:44424/club/graphql';
   final Logger logger = Logger();
 
   Future<Map<String, dynamic>?> createClub(String name) async {
-    try {
-      final response = await httpClient.post(
-        url,
-        data: {
-          'query': '''
-            mutation createMyClub(\$name: String!) {
-              createClub(name: \$name) {
+  try {
+    final response = await dioClient.post(
+      url,
+      data: {
+        'query': '''
+          mutation createMyClub(\$name: String!) {
+            createClub(name: \$name) {
+              id
+              name
+              members {
                 id
-                name
-                members {
-                  id
-                  role
-                }
+                role
               }
             }
-          ''',
-          'variables': {
-            'name': name,
-          },
+          }
+        ''',
+        'variables': {
+          'name': name,
         },
-        options: Options(headers: {
+      },
+      options: Options(
+        headers: {
           'Content-Type': 'application/json',
-        }),
-      );
+        },
+      ),
+    );
 
-      if (response.statusCode == 200 && response.data['data'] != null) {
-        return response.data['data']['createClub'];
-      } else {
-        logger.e('Ошибка при создании клуба: ${response.data}');
-        return null;
-      }
-    } catch (e, stackTrace) {
-      logger.e('Ошибка при выполнении запроса', error: e, stackTrace: stackTrace);
+    if (response.statusCode == 200 && response.data['data'] != null) {
+      return response.data['data']['createClub'];
+    } else {
+      logger.e('Ошибка при создании клуба',
+        error: {
+          'status': response.statusCode,
+          'data': response.data,
+          'requestPath': url,
+          'headers': response.headers.map,
+        },
+      );
       return null;
     }
+  } on DioException catch (e, stackTrace) {
+    logger.e('DioException во время запроса',
+      error: {
+        'message': e.message,
+        'statusCode': e.response?.statusCode,
+        'responseData': e.response?.data,
+        'requestPath': e.requestOptions.path,
+        'requestData': e.requestOptions.data,
+        'headers': e.requestOptions.headers,
+      },
+      stackTrace: stackTrace,
+    );
+    return null;
+  } catch (e, stackTrace) {
+    logger.e('Неизвестная ошибка при запросе', error: e, stackTrace: stackTrace);
+    return null;
   }
+}
 }
