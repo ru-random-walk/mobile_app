5eb63d2bf0aea0fe158fe451dc421a7ac3d05b26
Maxim Seleznev <maximuhkass14@gmail.com>
2025-05-24 13:01:06 +0300

    правки сущностей пользователя

--- auth/lib/src/data/models/user/detailed_user.dart
+++ auth/lib/src/data/models/user/detailed_user.dart
@@ -6,7 +6,6 @@ part 'detailed_user.g.dart';
 @JsonSerializable(createToJson: false)
 class DetailedUserModel extends UserModel {
   final String email;
-  final String? description;
   DetailedUserModel({
     required super.id,
     required super.fullName,
--- auth/lib/src/domain/entities/user/detailed.dart
+++ auth/lib/src/domain/entities/user/detailed.dart
@@ -2,12 +2,10 @@ import 'package:auth/src/domain/entities/user/user.dart';
 
 class DetailedUserEntity extends UserEntity {
   final String email;
-  final String? description;
   DetailedUserEntity({
     required super.id,
     required super.fullName,
     required this.email,
-    required this.description,
     required super.photoVersion,
     required super.description,
   });
