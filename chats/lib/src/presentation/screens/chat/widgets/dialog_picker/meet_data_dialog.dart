b8290be4418f0ccbe2ead996833921078973b05e
Maxim Seleznev <maximuhkass14@gmail.com>
2025-06-10 11:41:17 +0300

    фикс выбора начальной даты встречи в прошлом

--- chats/lib/src/presentation/screens/chat/widgets/dialog_picker/meet_data_dialog.dart
+++ chats/lib/src/presentation/screens/chat/widgets/dialog_picker/meet_data_dialog.dart
@@ -14,55 +14,88 @@ class _MeetDataDialogWidgetState extends State<_MeetDataDialogWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Dialog(
-      child: SizedBox(
-        height: 270.toFigmaSize,
-        width: 351.toFigmaSize,
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            color: context.colors.base_0,
-            borderRadius: BorderRadius.circular(16.toFigmaSize),
-          ),
-          child: Padding(
-            padding: EdgeInsets.all(8.toFigmaSize),
-            child: Center(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _MeetPickDataDialogCloseButton(),
-                  _MeetDataPickersGroupWidget(),
-                  SizedBox(
-                    height: 16.toFigmaSize,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 9.toFigmaSize,
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: GestureDetector(
+        onTap: () => Navigator.pop(context),
+        behavior: HitTestBehavior.opaque,
+        child: Center(
+          child: Dialog(
+            child: SizedBox(
+              height: 270.toFigmaSize,
+              width: 351.toFigmaSize,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: context.colors.base_0,
+                  borderRadius: BorderRadius.circular(16.toFigmaSize),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.all(8.toFigmaSize),
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _MeetPickDataDialogCloseButton(),
+                        _MeetDataPickersGroupWidget(),
+                        SizedBox(
+                          height: 16.toFigmaSize,
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 9.toFigmaSize,
+                          ),
+                          child: PickerConfirmButton(
+                            onTap: () {
+                              if (allFieldsFilled) {
+                                final pickedDate = selectedDate!.add(
+                                  Duration(
+                                    hours: selectedTime!.hour,
+                                    minutes: selectedTime!.minute,
+                                  ),
+                                );
+                                if (pickedDate.isBefore(DateTime.now())) {
+                                  final snackBar = SnackBar(
+                                    behavior: SnackBarBehavior.floating,
+                                    content: Text(
+                                      'Выбранная дата уже прошла',
+                                      style:
+                                          context.textTheme.bodyMRegularBase90,
+                                    ),
+                                    backgroundColor: context.colors.base_0,
+                                    duration:
+                                        const Duration(milliseconds: 1500),
+                                  );
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(snackBar);
+                                  return;
+                                }
+                                final invite = InviteEntity(
+                                  date: selectedDate!,
+                                  time: selectedTime!,
+                                  place: geolocation!,
+                                );
+                                Navigator.of(context).pop(invite);
+                              } else {
+                                final snackBar = SnackBar(
+                                  behavior: SnackBarBehavior.floating,
+                                  content: Text(
+                                    'Заполните все поля',
+                                    style: context.textTheme.bodyMRegularBase90,
+                                  ),
+                                  backgroundColor: context.colors.base_0,
+                                  duration: const Duration(milliseconds: 1500),
+                                );
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(snackBar);
+                              }
+                            },
+                          ),
+                        ),
+                      ],
                     ),
-                    child: PickerConfirmButton(
-                      onTap: () {
-                        if (allFieldsFilled) {
-                          final invite = InviteEntity(
-                            date: selectedDate!,
-                            time: selectedTime!,
-                            place: geolocation!,
-                          );
-                          Navigator.of(context).pop(invite);
-                        } else {
-                          final snackBar = SnackBar(
-                            content: Text(
-                              'Заполните все поля',
-                              style: context.textTheme.bodyMRegularBase90,
-                            ),
-                            backgroundColor: context.colors.base_0,
-                            duration: const Duration(milliseconds: 1500),
-                          );
-                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                        }
-                      },
-                    ),
                   ),
-                ],
+                ),
               ),
             ),
           ),
